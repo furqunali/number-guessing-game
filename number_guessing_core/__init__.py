ebe11ecec89f2292b0e0c04733b628ac2ecacf7e
Furqan Ali <furqanali2628@gmail.com>
2026-09-23 00:13:19 +0500

    API: define stable package exports (#8)
    * api: expose stable number-guessing public exports
    
    * fix: export only existing public symbols

--- number_guessing_core/__init__.py
+++ number_guessing_core/__init__.py
@@ -1,4 +1,14 @@
+"""Public API for the number-guessing domain package."""
+
 from .engine import GuessEngine, GuessResult
+from .limits import difficulty_for_limit, remaining_attempts, validate_attempt_limit
 from .stats import GuessStats
 
-__all__ = ["GuessEngine", "GuessResult", "GuessStats"]
+__all__ = [
+    "GuessEngine",
+    "GuessResult",
+    "GuessStats",
+    "difficulty_for_limit",
+    "remaining_attempts",
+    "validate_attempt_limit",
+]
