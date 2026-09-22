bd71f7ed249f2e90ce78e32c9e55c405fcb879b0
Furqan Ali <furqanali2628@gmail.com>
2026-09-23 00:00:21 +0500

    fix: export only existing public symbols

--- number_guessing_core/__init__.py
+++ number_guessing_core/__init__.py
@@ -2,14 +2,12 @@
 
 from .engine import GuessEngine, GuessResult
 from .limits import difficulty_for_limit, remaining_attempts, validate_attempt_limit
-from .scoring import calculate_score
 from .stats import GuessStats
 
 __all__ = [
     "GuessEngine",
     "GuessResult",
     "GuessStats",
-    "calculate_score",
     "difficulty_for_limit",
     "remaining_attempts",
     "validate_attempt_limit",
