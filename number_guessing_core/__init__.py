d9b6992dac6ed364cae7f2c20d15d59c22b803a4
Furqan Ali <furqanali2628@gmail.com>
2026-09-23 02:56:56 +0500

    Cycle 11: export history API

--- number_guessing_core/__init__.py
+++ number_guessing_core/__init__.py
@@ -1,16 +1,24 @@
 """Public API for the number-guessing domain package."""
 
 from .engine import GuessEngine, GuessResult
+from .feedback import feedback_distance, guess_feedback
+from .history import GuessHistory, GuessRecord
+from .leaderboard import LeaderboardEntry, rank_entries, validate_entry
 from .limits import difficulty_for_limit, remaining_attempts, validate_attempt_limit
 from .stats import GuessStats
 
 __all__ = [
     "GuessEngine",
     "GuessResult",
+    "GuessHistory",
+    "GuessRecord",
+    "LeaderboardEntry",
     "GuessStats",
     "difficulty_for_limit",
     "remaining_attempts",
     "validate_attempt_limit",
+    "feedback_distance",
+    "guess_feedback",
+    "rank_entries",
+    "validate_entry",
 ]
-
-from .leaderboard import validate_entry
