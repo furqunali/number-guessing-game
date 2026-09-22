d869fa3e703da09a410b300838ccdd5c583284d6
Furqan Ali <furqanali2628@gmail.com>
2026-09-22 23:58:35 +0500

    api: expose stable number-guessing public exports

--- number_guessing_core/__init__.py
+++ number_guessing_core/__init__.py
@@ -1,4 +1,16 @@
+"""Public API for the number-guessing domain package."""
+
 from .engine import GuessEngine, GuessResult
+from .limits import difficulty_for_limit, remaining_attempts, validate_attempt_limit
+from .scoring import calculate_score
 from .stats import GuessStats
 
-__all__ = ["GuessEngine", "GuessResult", "GuessStats"]
+__all__ = [
+    "GuessEngine",
+    "GuessResult",
+    "GuessStats",
+    "calculate_score",
+    "difficulty_for_limit",
+    "remaining_attempts",
+    "validate_attempt_limit",
+]
